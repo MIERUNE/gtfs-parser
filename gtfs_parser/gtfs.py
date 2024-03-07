38a711455675336acbae528cdf8fbefe0defeaa2
takohei <kohei-ota@t-brain.jp>
2024-03-08 04:15:35 +0900

    change: read a zip file without extracting
    To save the effort of decompressing files externally and to conserve storage.

--- gtfs_parser/gtfs.py
+++ gtfs_parser/gtfs.py
@@ -1,20 +1,36 @@
 import glob
 import os
-
+import zipfile
 import pandas as pd
+import io
+
+
+def append_table(f: io.BufferedIOBase, table_path: str, table_dfs: dict):
+    datatype = os.path.splitext(os.path.basename(table_path))[0]
+    df = pd.read_csv(f, dtype=str)
+    if len(df) == 0:
+        print(f"{datatype}.txt is empty, skipping...")
+        pass
+    table_dfs[datatype] = df
 
 
 def GTFS(gtfs_dir: str) -> dict:
     tables = {}
-    table_files = glob.glob(os.path.join(gtfs_dir, "*.txt"))
-    for table_file in table_files:
-        datatype = os.path.basename(table_file).split(".")[0]
-        with open(table_file, encoding="utf-8_sig") as f:
-            df = pd.read_csv(f, dtype=str)
-            if len(df) == 0:
-                print(f"{datatype}.txt is empty, skipping...")
-                continue
-            tables[datatype] = df
+    path = os.path.join(gtfs_dir)
+    if os.path.isdir(path):
+        table_files = glob.glob(os.path.join(gtfs_dir, "*.txt"))
+        for table_file in table_files:
+            with open(table_file, encoding="utf-8_sig") as f:
+                append_table(f, table_file, tables)
+    else:
+        try:
+            with zipfile.ZipFile(path) as z:
+                for file_name in z.namelist():
+                    with z.open(file_name) as f:
+                        append_table(f, file_name, tables)
+        except Exception as e:
+            print(f"zip file read error({path}: {str(e)}")
+            raise e
 
     # cast some numeric columns from str to numeric
     tables["stops"] = tables["stops"].astype({"stop_lon": float, "stop_lat": float})
