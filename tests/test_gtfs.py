f168cb3e73aa95a69c2cbed84159e85ed734ab7f
takohei <kohei-ota@t-brain.jp>
2024-05-06 03:26:00 +0900

    skip unused files, fix test

--- tests/test_gtfs.py
+++ tests/test_gtfs.py
@@ -12,23 +12,13 @@ def test_gtfs():
 
     # required tables
     assert isinstance(gtfs.agency, pd.DataFrame)
-    assert isinstance(gtfs.calendar, pd.DataFrame)
     assert isinstance(gtfs.routes, pd.DataFrame)
     assert isinstance(gtfs.stop_times, pd.DataFrame)
     assert isinstance(gtfs.stops, pd.DataFrame)
     assert isinstance(gtfs.trips, pd.DataFrame)
 
     # optional but in fixture
-    assert isinstance(gtfs.fare_attributes, pd.DataFrame)
-    assert isinstance(gtfs.fare_rules, pd.DataFrame)
     assert isinstance(gtfs.feed_info, pd.DataFrame)
-    assert isinstance(gtfs.office_jp, pd.DataFrame)
     assert isinstance(gtfs.shapes, pd.DataFrame)
-    assert isinstance(gtfs.translations, pd.DataFrame)
+    assert isinstance(gtfs.calendar, pd.DataFrame)
     assert isinstance(gtfs.calendar_dates, pd.DataFrame)
-
-    # optional but not in fixture
-    assert gtfs.agency_jp is None
-    assert gtfs.frequencies is None
-    assert gtfs.routes_jp is None
-    assert gtfs.transfers is None
